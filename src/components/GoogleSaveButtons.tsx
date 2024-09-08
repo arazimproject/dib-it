14dbf2a07825d3ce726efa698c038eb8187c554e
Arazim Project <144990276+arazonim@users.noreply.github.com>
2024-09-08 13:54:43 +0300

    add tooltips to google buttons, keep semester

--- src/components/GoogleSaveButtons.tsx
+++ src/components/GoogleSaveButtons.tsx
@@ -1,17 +1,10 @@
-import { Button } from "@mantine/core"
+import { Button, Tooltip } from "@mantine/core"
 import { notifications } from "@mantine/notifications"
 import { doc, getDoc, setDoc } from "firebase/firestore"
 import { useState } from "react"
 import { useAuthState } from "react-firebase-hooks/auth"
 import { auth, firestore } from "../firebase"
-import { setLocalStorage } from "../hooks"
 import { useDibIt } from "../models"
-
-export const restore = (json: any) => {
-  for (const key in json) {
-    setLocalStorage(key, json[key])
-  }
-}
 
 const GoogleSaveButtons = () => {
   const [currentUser] = useAuthState(auth)
@@ -25,79 +18,85 @@ const GoogleSaveButtons = () => {
 
   return (
     <Button.Group mb={10} style={{ width: "100%" }}>
-      <Button
-        color="green"
-        style={{ width: "50%" }}
-        leftSection={<i className="fa-solid fa-save" />}
-        loading={saveLoading}
-        onClick={() => {
-          setSaveLoading(true)
-          setDoc(doc(firestore, `/users/${currentUser.uid}`), dibIt, {
-            merge: true,
-          })
-            .then(() => {
-              setSaveLoading(false)
-              notifications.show({
-                title: "השמירה בגוגל בוצעה בהצלחה",
-                message: "המערכות שלכם זמינות כעת להורדה במכשירים אחרים",
-                style: { direction: "rtl" },
-                icon: <i className="fa-solid fa-check" />,
-                color: "green",
-              })
+      <Tooltip label="שימו לב - מה שנשמר בגוגל זה כל המערכות שלכם (ולא סמסטר ספציפי). פעולה זו תדרוס את כל מה ששמור כרגע!">
+        <Button
+          color="green"
+          style={{ width: "50%" }}
+          leftSection={<i className="fa-solid fa-save" />}
+          loading={saveLoading}
+          onClick={() => {
+            setSaveLoading(true)
+            setDoc(doc(firestore, `/users/${currentUser.uid}`), dibIt, {
+              merge: true,
             })
-            .catch((e) => {
-              setSaveLoading(false)
-              notifications.show({
-                title: "שגיאה בשמירה בגוגל",
-                message: "אנא נסו שנית. פרטי השגיאה: " + e?.message,
-                style: { direction: "rtl" },
-                icon: <i className="fa-solid fa-exclamation" />,
-                color: "red",
+              .then(() => {
+                setSaveLoading(false)
+                notifications.show({
+                  title: "השמירה בגוגל בוצעה בהצלחה",
+                  message: "המערכות שלכם זמינות כעת להורדה במכשירים אחרים",
+                  style: { direction: "rtl" },
+                  icon: <i className="fa-solid fa-check" />,
+                  color: "green",
+                })
               })
-            })
-        }}
-      >
-        שמירה בגוגל
-      </Button>
-      <Button
-        color="green"
-        style={{ width: "50%" }}
-        leftSection={<i className="fa-solid fa-sync" />}
-        loading={restoreLoading}
-        onClick={() => {
-          setRestoreLoading(true)
-          getDoc(doc(firestore, `/users/${currentUser.uid}`))
-            .then((d) => {
-              const data = d.data()
+              .catch((e) => {
+                setSaveLoading(false)
+                notifications.show({
+                  title: "שגיאה בשמירה בגוגל",
+                  message: "אנא נסו שנית. פרטי השגיאה: " + e?.message,
+                  style: { direction: "rtl" },
+                  icon: <i className="fa-solid fa-exclamation" />,
+                  color: "red",
+                })
+              })
+          }}
+        >
+          שמירה בגוגל
+        </Button>
+      </Tooltip>
+      <Tooltip label="שימו לב - מה שנשמר בגוגל זה כל המערכות שלכם (ולא סמסטר ספציפי). פעולה זו תדרוס את כל המערכות שלכם כרגע!">
+        <Button
+          color="green"
+          style={{ width: "50%" }}
+          leftSection={<i className="fa-solid fa-sync" />}
+          loading={restoreLoading}
+          onClick={() => {
+            setRestoreLoading(true)
+            getDoc(doc(firestore, `/users/${currentUser.uid}`))
+              .then((d) => {
+                const data = d.data()
 
-              if (data) {
-                restore(data)
-                setDibIt(data)
-              }
+                if (data) {
+                  setDibIt({
+                    ...data,
+                    semester: dibIt.semester ?? data.semester,
+                  })
+                }
 
-              notifications.show({
-                title: "העדכון מגוגל בוצע בהצלחה",
-                message: "המערכות שלכם התעדכנו בהתאם למה ששמור במשתמש שלכם",
-                style: { direction: "rtl" },
-                icon: <i className="fa-solid fa-check" />,
-                color: "green",
+                notifications.show({
+                  title: "העדכון מגוגל בוצע בהצלחה",
+                  message: "המערכות שלכם התעדכנו בהתאם למה ששמור במשתמש שלכם",
+                  style: { direction: "rtl" },
+                  icon: <i className="fa-solid fa-check" />,
+                  color: "green",
+                })
+                setRestoreLoading(false)
               })
-              setRestoreLoading(false)
-            })
-            .catch((e) => {
-              notifications.show({
-                title: "שגיאה בעדכון מגוגל",
-                message: "אנא נסו שנית. פרטי השגיאה: " + e?.message,
-                style: { direction: "rtl" },
-                icon: <i className="fa-solid fa-exclamation" />,
-                color: "red",
+              .catch((e) => {
+                notifications.show({
+                  title: "שגיאה בעדכון מגוגל",
+                  message: "אנא נסו שנית. פרטי השגיאה: " + e?.message,
+                  style: { direction: "rtl" },
+                  icon: <i className="fa-solid fa-exclamation" />,
+                  color: "red",
+                })
+                setRestoreLoading(false)
               })
-              setRestoreLoading(false)
-            })
-        }}
-      >
-        עדכון מגוגל
-      </Button>
+          }}
+        >
+          עדכון מגוגל
+        </Button>
+      </Tooltip>
     </Button.Group>
   )
 }
